d616a8852d74d17a1e47bc9561d18aeca9277df4
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-08-05 01:14:59 +0200

    Added a scrolling background the menu

--- MainMenuState.js
+++ MainMenuState.js
@@ -1,15 +1,27 @@
 function MainMenuState(){
-
+	this.bgtile = new Image();
 }
 
-MainMenuState.prototype.init = function(){}
+MainMenuState.prototype.init = function(){
+	this.bgtile.src = "gfx/bgtile.gif";
+	this.bgX = 0;
+	this.bgY = 0;
+}
 MainMenuState.prototype.pause= function(){}
 MainMenuState.prototype.resume= function(){}
 
 MainMenuState.prototype.render = function(ctx){
 	ctx.fillStyle = "white";
 	ctx.fillRect(0,0,16*GU,9*GU);
-	ctx.fillStyle = "black";
+
+	/* TODO: scale this, maybe? */ 
+	for(var x=0;x<17*GU;x+=this.bgtile.width){
+		for(var y=0;y<10*GU;y+=this.bgtile.height){
+			ctx.drawImage(this.bgtile,x-this.bgX,y-this.bgY);
+		}
+	}
+
+	ctx.fillStyle = "white";
 	ctx.font = (GU/2)+"px Arial";
 	ctx.fillText("This is the main menu. press enter to play",100,100);
 }
@@ -18,4 +30,7 @@ MainMenuState.prototype.update = function(){
 	if(KEYS[13]){
 		sm.changeState("game");
 	}	
+
+	this.bgX = (GU/100+this.bgX)%this.bgtile.width;
+	this.bgY = (GU/100+this.bgY)%this.bgtile.height;
 }
