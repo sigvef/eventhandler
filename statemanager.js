c2d2411787d9b8cde82ceedfb8a493326d1aec02
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-08-05 00:01:01 +0200

    Added statemanager.js

---
+++ statemanager.js
@@ -0,0 +1,24 @@
+
+function StateManager(){
+	this.states = {};
+	this.activeState;
+}
+
+StateManager.prototype.addState = function(name, state){
+	state.init();
+	this.states[name] = state;
+}
+
+StateManager.prototype.changeState = function(name){
+	this.activeState.pause();
+	this.activeState = this.states[name];
+	this.activeState.resume();
+}
+
+StateManager.prototype.render = function(ctx){
+	this.activeState.render(ctx);
+}
+
+StateManager.prototype.update = function(){
+	this.activeState.update();
+}
