ce132eb37c461472e48474e78e65b7ab3688c29c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-08-05 15:39:47 +0200

    Added post-processing effects

--- bootstrap.js
+++ bootstrap.js
@@ -29,6 +29,31 @@ function loop(){
     /* clearing canvas */
     canvas.width = canvas.width;
     sm.render(ctx);
+
+    /* post process scanlines */
+    ctx.drawImage(scanlinecanvas,0,0);
+    
+    /* post process glow */
+    blurcanvas.width = blurcanvas.width;
+    blurctx.scale(0.5,0.5);
+    blurctx.drawImage(canvas,0,0);
+    for(var i=0;i<4;i++){
+	    glowcanvas.width = glowcanvas.width;
+	    glowctx.scale(2,2);
+	    glowctx.drawImage(blurcanvas,0,0);
+	    blurcanvas.width = blurcanvas.width;
+	    blurctx.scale(0.5,0.5);
+	    blurctx.drawImage(glowcanvas,0,0);
+    }
+    ctx.save();
+    ctx.fillStyle = "rgba(0,0,0,0.5)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.globalCompositeOperation = "lighter";
+    ctx.globalAlpha = 0.5;
+    ctx.drawImage(glowcanvas,0,0);
+    ctx.restore();
+
+
     requestAnimFrame(loop);
 }
 
@@ -37,6 +62,14 @@ function bootstrap(){
 	/* global on purpose */
 	canvas = document.createElement("canvas");
 	ctx = canvas.getContext("2d");
+	canvas.style.zIndex = 999;
+	glowcanvas = document.createElement("canvas");
+	glowctx = glowcanvas.getContext("2d");
+	blurcanvas = document.createElement("canvas");
+	blurctx = blurcanvas.getContext("2d");
+	scanlinecanvas = document.createElement("canvas");
+	scanlinectx = scanlinecanvas.getContext("2d");
+	
 	sm = new StateManager();
 	mm = new MusicManager();
 	sfxm = new SFXManager();
@@ -79,6 +112,21 @@ function resize(e){
 	canvas.width = 16*GU;
 	canvas.height = 9*GU;
 	canvas.style.margin = ((window.innerHeight - 9*GU) /2)+"px 0 0 "+((window.innerWidth-16*GU)/2)+"px";
+	blurcanvas.width = 16*GU/2;
+	blurcanvas.height = 9*GU/2;
+	glowcanvas.width = 16*GU;
+	glowcanvas.height = 9*GU;
+	scanlinecanvas.width = 16*GU;
+	scanlinecanvas.height = 9*GU;
+	scanlinecanvas.style.margin = ((window.innerHeight - 9*GU) /2)+"px 0 0 "+((window.innerWidth-16*GU)/2)+"px";
+	scanlinectx.fillStyle = "rgba(0,0,0,0.05)";
+	for(var i=0;i<9;i++){
+		scanlinectx.fillRect(0,i*GU+0.0*GU,16*GU,0.1*GU);
+		scanlinectx.fillRect(0,i*GU+0.2*GU,16*GU,0.1*GU);
+		scanlinectx.fillRect(0,i*GU+0.4*GU,16*GU,0.1*GU);
+		scanlinectx.fillRect(0,i*GU+0.6*GU,16*GU,0.1*GU);
+		scanlinectx.fillRect(0,i*GU+0.8*GU,16*GU,0.1*GU);
+	}
 }
 
 window.onresize = resize;
